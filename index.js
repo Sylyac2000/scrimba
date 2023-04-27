bd7491d867e41b496c960ae834341a0a0d69ab90
sylyac2000 <sylyac2000@gmail.com>
2023-04-27 09:56:14 +0000

    01

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function MainContent() {
+    return(
+        <h1>Main</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        My name is SYL
+        <MainContent />
+    </div>,
+    document.getElementById('root'));
